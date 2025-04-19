8ea93acda0bbbd1e79f5bdd0a1b27c273bfaa9e8
talalcodes55 <talalalghazal55@gmail.com>
2025-04-19 20:40:05 +0300

    added fontawesome icon library

--- frontend/web/components/ui/githubAuthBtn.tsx
+++ frontend/web/components/ui/githubAuthBtn.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Button } from './button';
-import { Github } from "lucide-react";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function GithubAuthBtn() {
+export default function GithubAuthBtn({onClick} : {onClick?: () => void}) {
     return (
-        <Button variant={'default'} onClick={() => { }}>Continue with GitHub</Button>
+        <Button onClick={onClick ?? (() => {})} variant={'default'} className='font-[family-name:var(--font-geist-sans)] cursor-pointer'><FontAwesomeIcon icon={faGithub} size={'2x'} />Continue with GitHub</Button>
     );
 }

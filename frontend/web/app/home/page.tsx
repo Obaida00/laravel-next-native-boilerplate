6116d62258845e0aa9cf12340bb2246dbb2e5b8c
talalcodes55 <talalalghazal55@gmail.com>
2025-04-20 18:53:36 +0300

    added token handling for laravel

--- frontend/web/app/home/page.tsx
+++ frontend/web/app/home/page.tsx
@@ -2,12 +2,39 @@
 import { Button } from '@/components/ui/button';
 import useAuthCheck from '@/hooks/useAuthCheck'
 import { signOut, useSession } from 'next-auth/react';
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function Homepage() {
 
     useAuthCheck({ when: "unauthenticated", redirectTo: "/" });
-    const {data: session} = useSession();
+    const { data: session, status } = useSession();
+    useEffect(() => {
+        console.log(session?.accessToken);
+        const sendToLaravel = async () => {
+            if (!session?.accessToken) {
+                return;
+            }
+            const response = await fetch("http://localhost:8000/api/social/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    "provider": "github",
+                    "token": session.accessToken
+                }),
+            });
+
+            const data = await response.json();
+            if (data.token) {
+                localStorage.setItem("laravel_token", data.token);
+                console.log(data.token);
+            }
+        }
+        if (status === "authenticated" && !localStorage.getItem("laravel_token")) {
+            sendToLaravel();
+        }
+    }, [session, status])
     return (
         <div className="flex justify-center items-center min-h-screen">
             <h1 className='text-2xl'>Welcome, {session?.user?.name}</h1>

dc9c888385bf2e698ad2da5e9fea2f20689b0eed
talalcodes55 <talalalghazal55@gmail.com>
2025-04-27 10:20:33 +0300

    edits in the middleware

--- frontend/web/middleware.ts
+++ frontend/web/middleware.ts
@@ -4,15 +4,13 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
   console.log("my token is: " + token);
-  
+
   if (!token && request.nextUrl.pathname !== "/") {
     console.log("You cant go there");
-    
     return NextResponse.redirect(new URL("/", request.url));
-  } else {
-    console.log(token);
   }
 
+  console.log(token);
   return NextResponse.next();
 }
 

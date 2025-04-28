3aa10434dade7d20938fa40d1c3e215065a13a34
talalcodes55 <talalalghazal55@gmail.com>
2025-04-28 09:57:18 +0300

    reset the first route and created a home page in the mobile

--- frontend/mobile/app/index.tsx
+++ frontend/mobile/app/index.tsx
@@ -1,15 +1,37 @@
-import { Text, View } from "react-native";
+import { Button, ButtonText } from "@/components/ui/button";
+import { router } from "expo-router";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text style={styles.headerText}>Welcome to our pharmacy</Text>
+        <TouchableOpacity style={styles.gettingStartedBtn} onPress={() => router.push("./home")}>
+          <Text style={{ color: "green" }}>get started</Text>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  headerText: {
+    fontSize: 22,
+  },
+  gettingStartedBtn: {
+    backgroundColor: "white",
+    width: "80%",
+    height: 50,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+
+})

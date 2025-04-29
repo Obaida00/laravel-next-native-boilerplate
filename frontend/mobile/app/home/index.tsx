a8264a4d661a59e1175e556952e4a68ef237c926
talalcodes55 <talalalghazal55@gmail.com>
2025-04-29 18:54:31 +0300

    added the home screen and displayed the user info with a button to logout

--- frontend/mobile/app/home/index.tsx
+++ frontend/mobile/app/home/index.tsx
@@ -1,12 +1,20 @@
+import { Input } from '@/components/ui/input'
+import { useAuth } from '@/contexts/auth-context'
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 
 export default function Home() {
-  return (
-    <>
-        <View>
-            <Text>Welcome home</Text>
-        </View>
-    </>
-  )
+
+    const {user, logout} = useAuth()!;
+
+    return (
+        <>
+            <View>
+                <Text>Welcome Home, {user.name}</Text>
+                <TouchableOpacity onPress={logout}>
+                    <Text>Logout</Text>
+                </TouchableOpacity>
+            </View>
+        </>
+    )
 }
